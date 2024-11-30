c9443dbeb2fbc2c83bc71d35a5e0415377f5f2db
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-11-30 21:09:55 +0900

    Main 페이지 Slide 구현

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+const sliderData = [
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2015/12/09/17/11/vegetables-1085063_1280.jpg",
+    heading: "title 1",
+    desc: "description 1",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/09/13/17/03/technology-2746212_1280.jpg",
+    heading: "title 2",
+    desc: "description 2",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2023/03/29/17/05/artisanal-food-7885997_1280.jpg",
+    heading: "title 3",
+    desc: "description 3",
+  },
+];
+
+export default sliderData;
