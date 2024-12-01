be62607cea35456cac0f51931ec8227af1333b79
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-12-02 00:41:42 +0900

    useFetchCollection Hook을 이용한 데이터 fetch

--- src/hooks/useFetchCollection.jsx
+++ src/hooks/useFetchCollection.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
 
-const useFetchCollection = () => {
-  return <div></div>;
+import { useCallback, useEffect, useState } from "react";
+import { db } from "@/firebase/firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const useFetchCollection = (collectionName) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getCollection = useCallback(() => {
+    setIsLoading(true);
+
+    try {
+      const docRef = collection(db, collectionName);
+      const q = query(docRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        const allData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData(allData);
+        setIsLoading(false);
+      });
+    } catch (e) {
+      setIsLoading(false);
+      toast.error(e.message);
+    }
+  }, [collectionName]);
+
+  useEffect(() => {
+    getCollection();
+  }, [getCollection]);
+
+  return { data, isLoading };
 };
 
 export default useFetchCollection;
